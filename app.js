70c256f8031b37c96f6882ea240a2efde766449a
Jeffer <jefferd2016@gmail.com>
2022-05-29 19:16:05 -0500

    Add app.user for category and user

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 import express from "express";
 import morgan from "morgan";
-
 import operationRoutes from "./app/Routes/OperationRoutes";
+import categoryRoutes from "./app/Routes/CategoryRoutes";
+import userRoutes from "./app/Routes/UserRoutes";
 
+var cors = require('cors');
 const app = express();
 
 app.set('port' , 8080);
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors());
 
-app.use('/api/v1/home',operationRoutes);
+app.use('/api/v1/operations',operationRoutes);
+app.use('/api/v1/categories',categoryRoutes);
+app.use('/api/v1/users',userRoutes);
 
 export default app;
