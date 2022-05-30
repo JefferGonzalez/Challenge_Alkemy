c1f173641ba2f6d55c0a44fa7355a0cbb16dcd5a
Jeffer <jefferd2016@gmail.com>
2022-05-29 19:16:59 -0500

    Modified route for getAll operations

--- app/Routes/OperationRoutes.js
+++ app/Routes/OperationRoutes.js
@@ -3,7 +3,7 @@ import {actions as OperationController} from "../Controllers/OperationController
 
 const router = Router();
 
-router.get('/', OperationController.getAll);
+router.get('/user/:email', OperationController.getAll);
 router.get('/:id', OperationController.searchForId);
 router.post('/', OperationController.addOperation);
 router.put('/:id', OperationController.updateOperation);
