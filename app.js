70bba81e690403b8607b40e767432f19d736dece
Jeffer <jefferd2016@gmail.com>
2022-05-29 20:05:40 -0500

    Add path for connection with react

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import userRoutes from "./app/Routes/UserRoutes";
 
 var cors = require('cors');
 const app = express();
+const path = require('path');
 
 app.set('port' , 8080);
 
@@ -13,6 +14,10 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.use('/api/v1/operations',operationRoutes);
 app.use('/api/v1/categories',categoryRoutes);
 app.use('/api/v1/users',userRoutes);
